ade8951d3d1b30edebd7b24b2175ac90b8cf0997
RobTheWorld <rob@estevensons.plus.com>
2024-01-24 14:32:56 +0000

    add comment

--- scrape.py
+++ scrape.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-
+# testing git update
 res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
 print(soup.find(id='score_38851337'))
